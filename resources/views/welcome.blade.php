da5f57772c640c6e5b564267246f6c7287e4a340
Rahul Kandel <rahulkandel7@gmail.com>
2022-10-30 11:43:20 +0545

    contact updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -52,7 +52,7 @@
             Nepalilainen Ravintola Fusion Himalayan, Authentic Nepalese restaurant in the Yrjonkatu 7 Pori. 
             Fusion Himalayan offers menus for Lunch and Dinner. The restaurant has a Latin atmosphere with Latin American ornaments and music. Within a year of its opening, Fusion Himalayan has been recognized for its quality of food and excellent service.
           </p>
-          <p class="text-justify text-gray-600 text-sm">
+          <p class="text-justify text-gray-600 text-sm mt-3">
             We are a close-knit, fun-loving, passionate group spreading the word about good times and wonderful cuisine. In our restaurant cooking space, we offer limited capacity onsite. We cook, tell tales, give interesting culinary demonstrations and lectures, but most importantly, we handle every customer as if you were a guest at our dinner table. Come with us!
             Come dine with us & experience an authentic food in an intimate dining space. We look forward to serving you!
           </p>
@@ -71,12 +71,7 @@
               <img src="{{asset('images/slide1.webp')}}" alt="" class="rounnded-lg  shadow-md">
             </div>
 
-            <div class="border border-indigo-200 p-2 rounded-lg shadow-lg">
-              <img src="{{asset('images/slide1.webp')}}" alt="" class="rounnded-lg  shadow-md">
-            </div>
-            <div class="border border-indigo-200 p-2 rounded-lg shadow-lg">
-              <img src="{{asset('images/slide1.webp')}}" alt="" class="rounnded-lg  shadow-md">
-            </div>
+            
         </div>
     </div>
   </div>
