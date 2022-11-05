17346f0bb4ae7bf4f11b376bd919f1c246fd5694
Rahul Kandel <rahulkandel7@gmail.com>
2022-11-05 10:27:36 +0545

    change slider

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -509,24 +509,12 @@
     <script>
         var swiper5 = new Swiper(".mySwiper5", {
             grabCursor: true,
-            effect: "creative",
+
             autoplay: {
                 delay: 2500,
                 disableOnInteraction: false,
             },
-            creativeEffect: {
-                prev: {
-                    shadow: true,
-                    origin: "left center",
-                    translate: ["-5%", 0, -200],
-                    rotate: [0, 100, 0],
-                },
-                next: {
-                    origin: "right center",
-                    translate: ["5%", 0, -200],
-                    rotate: [0, -100, 0],
-                },
-            },
+
         });
     </script>
 
